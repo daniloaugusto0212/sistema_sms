9acadbb7bf09b3b007c1e2ff7a835a3416504496
Danilo <daniloaugusto0212@hotmail.com>
2020-08-19 20:31:39 -0300

    Sistema Finalizado

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sistema_sms
-Sitema de envio de SMS usando API twilio
+Sistema de envio de SMS usando API twilio e foco no Back-End com PHP
 
 Instalar twilio via composer usando o comando: composer require twilio/sdk
 
-
+Criar uma conta em https://www.twilio.com/
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     require('vendor/autoload.php');
-
+    // Use the REST API Client to make requests to the Twilio REST API
+    use Twilio\Rest\Client;
 
 ?>
 
@@ -13,12 +14,43 @@
     <title>Projeto SMS</title>
 </head>
 <body>
-    
+    <?php
+        if (isset($_POST['acao'])) {
+            $numero = $_POST['numero'];
+            $mensagem = $_POST['mensagem'];
+
+            if (preg_match('/[0-9]{9,10}/', $numero)) {
+                if (preg_match('/[a-z,0-9]{1,255}/',$mensagem)) {
+                    // Your Account SID and Auth Token from twilio.com/console
+                    $sid = 'AC3242dd59bc447a1ceb97c3c1d091b986';
+                    $token = 'dff650da1545f29704b9eb10799cf7b8';
+                    $client = new Client($sid, $token);
+
+                    // Use the client to do fun stuff like send text messages!
+                    $client->messages->create(
+                        // the number you'd like to send the message to
+                        '+55'.$numero,
+                        [
+                            // A Twilio phone number you purchased at twilio.com/console
+                            'from' => '+17606915322',
+                            // the body of the text message you'd like to send
+                            'body' => $mensagem
+                        ]
+                    );
+                    echo '<script>alert("A mensagem foi enviada com sucesso!")</script>';
+                }else{
+                    echo '<script>alert("Use entre 1 e 255 caracteres!")</script>';
+                }
+            }else{
+                echo '<script>alert("Número inválido!")</script>';
+            }
+        }
+    ?>
     <section class="contato">
         <h1>Envio de SMS</h1>
         <form method="post">
             <input type="text" name="numero" placeholder="Destinatário...">
-            <textarea name="body" placeholder="Sua mensagem..."></textarea>
+            <textarea name="mensagem" placeholder="Sua mensagem..."></textarea>
             <input type="submit" name="acao" value="Enviar!">
         </form>
     </section>
